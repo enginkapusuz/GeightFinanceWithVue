5a8b8a1e078a69d5f99d8cbad33186c71de39ea7
engin kapusuz <ekapusuz@hotmail.com>
2023-06-29 13:40:54 +0300

    June-01

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>Hello {{ message }}</div>
+    <div>{{ message }}</div>
 </template>
 
 <script>
-export default {
-  name: "App",
-  data() {
-    return {
-      message: "World"
-    };
-  }
-},
-async mounted(){
-  const { text } = await (await fetch("/message")).json();
-  this.message = text;
+export default{
+    name:"App",
+    data(){
+        return {
+            message: "Engin KAPUSUZ"
+        };
+    },
+    async mounted() {
+        const { text } = await (await fetch("/api/message")).json();
+        this.message = text;
+    }
 };
 </script>
